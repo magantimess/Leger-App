a2057218a3d53b6e88aee8be8305e914d5e99faf
[dyad] <magantimess@gmail.com>
2026-02-27 23:42:06 +0530

    [dyad] Cleaning up Firebase configuration - wrote 1 file(s)

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,6 @@
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCCMafBfl9bd722jUWo6of2IkW778_cUeo",
   authDomain: "mangantimess.firebaseapp.com",
@@ -21,4 +13,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
