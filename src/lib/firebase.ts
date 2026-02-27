d271858334cd496ecc702c70e918cdb01033b262
[dyad] <magantimess@gmail.com>
2026-02-27 23:50:09 +0530

    Updated src/lib/firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
   apiKey: "AIzaSyCCMafBfl9bd722jUWo6of2IkW778_cUeo",
   authDomain: "mangantimess.firebaseapp.com",
@@ -10,7 +11,6 @@ const firebaseConfig = {
   appId: "1:129367193535:web:223fd5ecce1354158e530a",
   measurementId: "G-Q8QKB42TLP"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
